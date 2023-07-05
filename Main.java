abfc30dd6f17c840965831c36837580a495ac65a
LEGION <adietk@bk.ru>
2023-07-06 01:05:56 +0600

    Add code to solve problem 1

---
+++ Main.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    // Problem 1: Finding the minimum element in an array
+    public static int findMinimum(int[] arr){
+        int min = arr[0];
+        for(int i=1; i<arr.length; i++){
+            if(arr[i]<min){
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        // Problem 1: Finding the minimum element in an array
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+        int[] arr1 = new int[n];
+        System.out.print("Enter the elements: ");
+        for (int i = 0; i < n; i++) {
+            arr1[i] = scanner.nextInt();
+        }
+        int minElement = findMinimum(arr1);
+        System.out.println(minElement);
+    }
+}
