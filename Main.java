cf82112a56cee49eba6b8cbe74639d525317261b
LEGION <adietk@bk.ru>
2023-07-06 02:22:29 +0600

    Add code to solve problem 7

--- Main.java
+++ Main.java
@@ -59,6 +59,17 @@ public class Main {
         return a * power(a, n - 1);
     }
 
+    // Problem 7: Reversing an array using recursion
+    public static void reverseArray(int[] arr, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        reverseArray(arr, start + 1, end - 1);
+    }
+
     // Running code
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
@@ -109,5 +120,19 @@ public class Main {
         int exponent = scanner.nextInt();
         int powerResult = power(base, exponent);
         System.out.println(powerResult);
+
+        // Problem 7: Reversing an array using recursion
+        System.out.print("Enter the number of elements: ");
+        n = scanner.nextInt();
+        int[] arr3 = new int[n];
+        System.out.print("Enter the elements: ");
+        for (int i = 0; i < n; i++) {
+            arr3[i] = scanner.nextInt();
+        }
+        reverseArray(arr3, 0, arr3.length - 1);
+        for (int num : arr3) {
+            System.out.print(num + " ");
+        }
+
     }
 }
